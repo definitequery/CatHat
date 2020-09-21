faaf726dab2da340ee8d370eca3104e6211c527c
aaron-simpson97 <business.feznet@gmail.com>
2020-09-21 18:02:45 -0500

    Initial work towards MVP. Displaying static files. Server running. Logging middleware.

---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+function appLogger(request, response) {
+    console.log(`    Application ${process.env.APP_NAME} v${process.env.APP_VERS} on port ${process.env.APP_PORT} received request
+    |   Type: ${request.protocol}
+    |   Method: ${request.method}
+    |   Timestamp: ${new Date().toISOString()}`);
+    request.next();
+}
+
+module.exports = appLogger;
--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+require('dotenv').config();
+const appLogger = require('./middleware/logger');
+const express = require('express');
+app = express();
+
+app.use('/', express.static('static'));
+app.use(appLogger);
+app.listen(process.env.APP_PORT, () => console.log(`${process.env.APP_NAME} v${process.env.APP_VERS} 
+Running on Port ${process.env.APP_PORT}`));
