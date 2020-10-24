d02096f2c9be316da7ebc58853c6dc6927a76022
aaron-simpson97 <business.feznet@protonmail.com>
2020-10-23 19:53:46 -0500

    Changing template engine. Put off handling login and sessions in favor of more important MVP features.

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,5 +1,6 @@
 function appLogger(request, response) {
     console.log(`    Application ${process.env.APP_NAME} v${process.env.APP_VERS} on port ${process.env.APP_PORT} received request
+    |   Client IP: ${request.ip}
     |   Type: ${request.protocol}
     |   Method: ${request.method}
     |   Timestamp: ${new Date().toISOString()}`);
--- server.js
+++ server.js
@@ -1,118 +1,46 @@
 require('dotenv').config();
-const mustacheExpress = require('mustache-express');
+const exphbs = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const appLogger = require('./middleware/logger');
-const express = require('express');
-const session = require('express-session');
-const uuid = require('uuid');
-const pgSession = require('connect-pg-simple')(session);
 const db = require('./database/database');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const express = require('express');
 app = express();
 
-app.engine('html', mustacheExpress());
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views')
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(appLogger);
-app.use(session({
-    genid: (req) => uuid.v4(),
-    store: new pgSession({
-        pool: db,
-        tableName: "UserSessions"
-    }),
-    cookie: { maxAge: 60000 },
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-}))
 
-app.get('/', (req, res) => {
-    if (req.session.loggedin) {
-        res.render('courses', {firstName: req.session.first_name, lastName: req.session.last_name, instructor: req.session.is_instructor});
-    } else {
-        res.render('index');
-    }
-});
-app.get('/signup', (req, res) => res.render('sign_up'));
-app.post('/signup', (req, res) => {
-    const query = `INSERT INTO "Users"(school_name, first_name, last_name, email, password_hash, password_salt, is_instructor)
-        VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-    bcrypt.genSalt(saltRounds, (error, salt) => {
-        if (error) throw error;
-        bcrypt.hash(req.body.password, salt, (error, hash) => {
-            if (error) throw error;
-            const newUser = [req.body.school_name, req.body.first_name, req.body.last_name, 
-                req.body.email, hash, salt, req.body.is_instructor];
-            db.query(query, newUser, (error, result) => {
-                if (error) throw error;
-                req.session.loggedin = true;
-                req.session.first_name = req.body.first_name;
-                req.session.last_name = req.body.last_name;
-                res.render('courses', {firstName: req.session.first_name, lastName: req.session.last_name, instructor: req.session.is_instructor});
-            });
-        });
+app.get('/', (req, res) => res.render('home'));
+app.get('/courses/:user_id', (req, res) => {
+    const query = `SELECT * FROM "Courses" C INNER JOIN "Courses_Users" CU ON CU.course_id = C.course_id WHERE CU.user_id = $1`;
+    db.query(query, [req.params.user_id], (error, result) => {
+        if (error) {
+            res.send("Data retrieval failed!");
+            throw error;
+        } else {
+            res.render('courses', {courses: result.rows});
+        };
     });
 });
-app.get('/signin', (req, res) => res.render('sign_in'));
-app.post('/signin', (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const query = `SELECT * FROM "Users" WHERE email=$1`;
-    if (email && password) {
-        db.query(query, [email], (error, results) => {
-            if (results) {
-                bcrypt.compare(password, results.rows[0].password_hash, (error, result) => {
-                    if (result) {
-                        req.session.loggedin = true;
-                        req.session.first_name = results.rows[0].first_name;
-                        req.session.last_name = results.rows[0].last_name;
-                        req.session.is_instructor = results.rows[0].is_instructor;
-                        res.render('courses', {firstName: req.session.first_name, lastName: req.session.last_name, instructor: req.session.is_instructor});
-                    } else {
-                        res.render('sign_in', {error: true});
-                    }
-                });
-            } else {
-                res.render('sign_in', {error: true});
-            }
-        });
-    } else {
-        res.render('sign_in', {error: true});
-    }
-});
-app.get('/courses', (req, res) => {
-    if (req.session.loggedin) {
-        res.render('courses', {firstName: req.session.first_name, lastName: req.session.last_name, instructor: req.session.is_instructor});
-    } else {
-        res.redirect('/signin');
-    }
-});
 app.post('/courses', (req, res) => {
-    if (req.session.instructor) {
-        const query1 = `INSERT INTO "Courses" (department_abbr, course_name, course_number, semester, year, join_code))
-            VALUES ($1, $2, $3, $4, $5, $6)`;
-        const query2 = `INSERT INTO "Courses_Instructors" (instructor_id, course_id))
-        VALUES ($1, $2)`;
-        db.query(query1, newCourse, (error, result) => {
-            if (error) throw error;
-            console.log(result);
-        });
-    }
+    const course = [req.body.course_name, req.body.start_date, req.body.end_date, req.body.subject, 
+        req.body.course_code, req.body.description, req.body.join_code];
+    const query = `INSERT INTO "Courses"(course_name, start_date, end_date, subject, course_code, 
+        description, join_code) VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+    db.query(query, course, (error, result) => {
+        if (error) {
+            res.send("Data insertion failed!");
+            throw error;
+        } else {
+            res.send(`Data insertion completed successfuly! - ${JSON.stringify(result)}`);
+        };
+    });
 });
-app.get('/signout', (req, res) => {
-    if (req.session.loggedin) {
-        req.session.destroy((error) => {
-            if (error) throw error;
-            res.redirect('/');
-        });
-    }
-})
 
 app.listen(process.env.APP_PORT || 8080, () => console.log(`${process.env.APP_NAME} v${process.env.APP_VERS} 
 Running on Port ${process.env.APP_PORT}`));
